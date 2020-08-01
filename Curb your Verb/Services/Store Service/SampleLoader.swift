e57ad82b1983a576fbca226765c76f5d6e595d4d
Kavars <kavars@yandex.ru>
2020-08-01 17:27:39 +0300

    Create Sample Loader

---
+++ Curb your Verb/Services/Store Service/SampleLoader.swift
@@ -0,0 +1,69 @@
+//
+//  SampleLoader.swift
+//  Curb your Verb
+//
+//  Created by Kirill Varshamov on 01.08.2020.
+//  Copyright © 2020 Kirill Varshamov. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+protocol SampleLoaderProtocol: class {
+    func insertSampleData(to managedContext: NSManagedObjectContext)
+}
+
+class SampleLoader: SampleLoaderProtocol {
+    // MARK: - Sample Verbs
+    func insertSampleData(to managedContext: NSManagedObjectContext) {
+        
+        let fetch: NSFetchRequest<Verb> = Verb.fetchRequest()
+        
+        let count = try! managedContext.count(for: fetch)
+        
+        if count > 0 {
+            // SampleData.plist data already in CoreData
+            return
+        }
+        
+        let path = Bundle.main.path(forResource: "SampleData", ofType: "plist")
+        let dataArra = NSArray(contentsOfFile: path!)
+        
+        let dataArray = dataArra!
+        
+        for dict in dataArray {
+            let entityVerb = NSEntityDescription.entity(forEntityName: "Verb", in: managedContext)!
+            let entityVerbProgress = NSEntityDescription.entity(forEntityName: "VerbProgress", in: managedContext)!
+            
+            let verb = Verb(entity: entityVerb, insertInto: managedContext)
+            verb.progress = VerbProgress(entity: entityVerbProgress, insertInto: managedContext)
+            
+            let verbDict = dict as! [String: Any]
+            
+            verb.infinitive = verbDict["infinitive"] as? String
+            verb.pastSimple = verbDict["pastSimple"] as? String
+            verb.pastParticiple = verbDict["pastParticiple"] as? String
+            
+            verb.infinitiveTranscription = verbDict["infinitiveTranscription"] as? String
+            verb.pastSimpleTranscription = verbDict["pastSimpleTranscription"] as? String
+            verb.pastParticipleTranscription = verbDict["pastParticipleTranscription"] as? String
+            
+            verb.translation = verbDict["translation"] as? String
+            
+            let variants: [String]? = verbDict["variants"] as? [String]
+            
+            verb.variants = variants as NSObject?
+            
+            verb.isLearn = true
+            
+            verb.progress?.rightAnswersToday = 0
+            verb.progress?.wrongAnswersToday = 0
+            verb.progress?.rightAnswersForAllTime = 0
+            verb.progress?.wrongAnswersForAllTime = 0
+        }
+        
+        try! managedContext.save()
+        
+    }
+}
--- Curb your Verb/Services/Store Service/StoreService.swift
+++ Curb your Verb/Services/Store Service/StoreService.swift
@@ -16,7 +16,7 @@ protocol StoreServiceVerbsProtocol {
     
     var managedContext: NSManagedObjectContext { get }
     
-    func updateContext()
+    func updateContext() // ?
     
     func verbsFetch(of type: FetchType) -> [Verb]?
     
@@ -29,6 +29,10 @@ class StoreServiceCoreData: StoreServiceVerbsProtocol {
     init(modelName: String) {
         self.modelName = modelName
     }
+    
+    lazy var sampleLoader: SampleLoaderProtocol = {
+        return SampleLoader()
+    }()
     
     lazy var managedContext: NSManagedObjectContext = {
         return self.storeContainer.viewContext
@@ -46,6 +50,7 @@ class StoreServiceCoreData: StoreServiceVerbsProtocol {
         return container
     }()
     
+    // ?
     func updateContext() {
         let container = NSPersistentContainer(name: self.modelName)
         
@@ -57,6 +62,7 @@ class StoreServiceCoreData: StoreServiceVerbsProtocol {
         
         self.storeContainer = container
     }
+    //
     
     func saveContext() {
         guard managedContext.hasChanges else {
@@ -73,7 +79,9 @@ class StoreServiceCoreData: StoreServiceVerbsProtocol {
     func verbsFetch(of type: FetchType) -> [Verb]? {
         
         var verbs: [Verb] = []
-        insertSampleData()
+        
+        // Load sample verbs
+        sampleLoader.insertSampleData(to: self.managedContext)
         
         let fetch: NSFetchRequest<Verb> = Verb.fetchRequest()
         var predicate: NSPredicate?
@@ -107,58 +115,6 @@ class StoreServiceCoreData: StoreServiceVerbsProtocol {
         }
         
         return nil
-    }
-    
-    // MARK: - Sample Verbs
-    func insertSampleData() {
-        
-        let fetch: NSFetchRequest<Verb> = Verb.fetchRequest()
-        
-        let count = try! managedContext.count(for: fetch)
-        
-        if count > 0 {
-            // SampleData.plist data already in CoreData
-            return
-        }
-        
-        let path = Bundle.main.path(forResource: "SampleData", ofType: "plist")
-        let dataArra = NSArray(contentsOfFile: path!)
-        
-        let dataArray = dataArra!
-        
-        for dict in dataArray {
-            let entityVerb = NSEntityDescription.entity(forEntityName: "Verb", in: managedContext)!
-            let entityVerbProgress = NSEntityDescription.entity(forEntityName: "VerbProgress", in: managedContext)!
-            
-            let verb = Verb(entity: entityVerb, insertInto: managedContext)
-            verb.progress = VerbProgress(entity: entityVerbProgress, insertInto: managedContext)
-            
-            let verbDict = dict as! [String: Any]
-            
-            verb.infinitive = verbDict["infinitive"] as? String
-            verb.pastSimple = verbDict["pastSimple"] as? String
-            verb.pastParticiple = verbDict["pastParticiple"] as? String
-            
-            verb.infinitiveTranscription = verbDict["infinitiveTranscription"] as? String
-            verb.pastSimpleTranscription = verbDict["pastSimpleTranscription"] as? String
-            verb.pastParticipleTranscription = verbDict["pastParticipleTranscription"] as? String
-            
-            verb.translation = verbDict["translation"] as? String
-            
-            let variants: [String]? = verbDict["variants"] as? [String]
-            
-            verb.variants = variants as NSObject?
-            
-            verb.isLearn = true
-            
-            verb.progress?.rightAnswersToday = 0
-            verb.progress?.wrongAnswersToday = 0
-            verb.progress?.rightAnswersForAllTime = 0
-            verb.progress?.wrongAnswersForAllTime = 0
-        }
-        
-        try! managedContext.save()
-        
     }
     
     func resetStats() {
